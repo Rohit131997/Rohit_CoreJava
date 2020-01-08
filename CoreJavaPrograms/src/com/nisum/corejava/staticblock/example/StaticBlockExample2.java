226674ea726649fb4bfd4b2194d01e9865fba91a
Rohit131997 <59564737+Rohit131997@users.noreply.github.com>
2020-01-08 15:46:39 +0530

    Add

--- CoreJavaPrograms/src/com/nisum/corejava/staticblock/example/StaticBlockExample2.java
+++ CoreJavaPrograms/src/com/nisum/corejava/staticblock/example/StaticBlockExample2.java
@@ -2,13 +2,11 @@ package com.nisum.corejava.staticblock.example;
 
 public class StaticBlockExample2 {
     // filename: Main.java
-        static int i;
-        int j;
+        static int data1;
         static {
-            i = 10;
             System.out.println("static block called ");
         }
-    StaticBlockExample2(){
+   public StaticBlockExample2(){
             System.out.println("Constructor called");
         }
     }
@@ -17,8 +15,9 @@ public class StaticBlockExample2 {
         public static void main(String args[]) {
 
             // Although we have two objects, static block is executed only once.
-            StaticBlockExample2 t1 = new StaticBlockExample2();
-            StaticBlockExample2 t2 = new StaticBlockExample2();
+            StaticBlockExample2 obj1 = new StaticBlockExample2();
+            StaticBlockExample2 obj2 = new StaticBlockExample2();
+
         }
     }
 
