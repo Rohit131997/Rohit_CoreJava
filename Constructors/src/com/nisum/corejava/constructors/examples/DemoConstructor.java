f4a5d3a83c20f14a9034f0e66fae6e9269cb2719
Rohit131997 <59564737+Rohit131997@users.noreply.github.com>
2020-01-08 17:27:46 +0530

    Add

--- Constructors/src/com/nisum/corejava/constructors/examples/DemoConstructor.java
+++ Constructors/src/com/nisum/corejava/constructors/examples/DemoConstructor.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package com.nisum.corejava.constructors.examples;
 
@@ -9,34 +9,34 @@ package com.nisum.corejava.constructors.examples;
  */
 
 public class DemoConstructor {
-	int value1;
-	double value2;
+    int value1;
+    double value2;
 
-//Create a class constructor for the DemoConstructor class
-	public DemoConstructor()// default constructor
-	{
-		this.value1 = 5;
-		System.out.println("Default Constructor");
-	}
+    //Create a class constructor for the DemoConstructor class
+    public DemoConstructor()// default constructor
+    {
+        this.value1 = 5;
+        System.out.println("Default Constructor");
+    }
 
-	public DemoConstructor(int value3)// parameterized constructor with Integer argument
-	{
-		this.value1 = value3;
-		System.out.println("Constructor with Integer argument");
-	}
+    public DemoConstructor(int value3)// parameterized constructor with Integer argument
+    {
+        this.value1 = value3;
+        System.out.println("Constructor with Integer parameter");
+    }
 
-	public DemoConstructor(int value4, double value5)// parameterized constructor with Integer and Double argument
-	{
-		this.value1 = value4;
-		this.value2 = value5;
-		System.out.println("Constructor with Integer and Double argument");
+    public DemoConstructor(int value4, double value5)// parameterized constructor with Integer and Double argument
+    {
+        this.value1 = value4;
+        this.value2 = value5;
+        System.out.println("Constructor with Integer and Double parameters");
 
-	}
+    }
 
-	 static public void  main(String[] args) {
-		DemoConstructor d1 = new DemoConstructor(21,10.6);// passing the value to constructor
+    static public void main(String[] args) {
+        DemoConstructor d1 = new DemoConstructor(21, 10.6);// passing the value to constructor
 
-		System.out.println(d1.value1);
-		System.out.println(d1.value2);
-	}
+        System.out.println(d1.value1);
+        System.out.println(d1.value2);
+    }
 }
