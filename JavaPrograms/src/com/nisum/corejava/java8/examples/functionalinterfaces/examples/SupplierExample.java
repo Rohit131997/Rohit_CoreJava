4ea09c47ba0054434828a40233a785836a3fe29e
Rohit131997 <59564737+Rohit131997@users.noreply.github.com>
2020-01-29 15:37:31 +0530

    Adding

---
+++ JavaPrograms/src/com/nisum/corejava/java8/examples/functionalinterfaces/examples/SupplierExample.java
@@ -0,0 +1,17 @@
+package com.nisum.corejava.java8.examples.functionalinterfaces.examples;
+
+import com.nisum.corejava.util.MyLogger;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Supplier;
+
+public class SupplierExample {
+    public static void main(String[] args) {
+
+        Supplier<String> supplierobj = () -> "No elements Present ";
+
+        List<String> listobj = Arrays.asList();
+        MyLogger.consoleLogger.info( listobj.stream().findAny().orElseGet( supplierobj ) );
+    }
+}
