dd469366b2b0ca7ede13b73210dd856a5195bec0
Rohit131997 <59564737+Rohit131997@users.noreply.github.com>
2020-02-03 14:47:18 +0530

    Adding

---
+++ JavaPrograms/src/com/nisum/corejava/java8/examples/Streams/examples/Min_and_Max_example.java
@@ -0,0 +1,30 @@
+package com.nisum.corejava.java8.examples.Streams.examples;
+
+import com.nisum.corejava.util.MyLogger;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Min_and_Max_example {
+    public static void main(String[] args) {
+
+        List<Integer> numbers = Arrays.asList( 11, 43, 22, 57, 89 );
+
+        // Minimum and maximum in default sorting order
+        int minimum_ascending = numbers.stream().min( (value1, value2) -> value1.compareTo( value2 ) ).get();
+        MyLogger.consoleLogger.info( "Minimum value in ascending order: " + minimum_ascending );
+
+        int maximum_ascending = numbers.stream().max( (value1, value2) -> value1.compareTo( value2 ) ).get();
+        MyLogger.consoleLogger.info( "Maximum value in ascending order: " + maximum_ascending );
+
+
+        // Minimum and maximum in userdefined sorting order
+        int minimum_desending = numbers.stream().min( (value1, value2) -> value2.compareTo( value1 ) ).get();
+        MyLogger.consoleLogger.info( "Minimum value in Desending order: " + minimum_desending );
+
+        int maximum_desending = numbers.stream().max( (value1, value2) -> value2.compareTo( value1 ) ).get();
+        MyLogger.consoleLogger.info( "Maximum value in Desending order: " + maximum_desending );
+
+
+    }
+}
