4e0e846fc7a11b46734c4d9ebdf5ae73cf4b74c1
Rohit131997 <59564737+Rohit131997@users.noreply.github.com>
2020-02-03 18:45:27 +0530

    Adding

---
+++ JavaPrograms/src/com/nisum/corejava/java8/examples/Timeapi/examples/OffSetTime_example.java
@@ -0,0 +1,23 @@
+package com.nisum.corejava.java8.examples.Timeapi.examples;
+
+import com.nisum.corejava.util.MyLogger;
+
+import java.time.OffsetTime;
+
+public class OffSetTime_example {
+    public static void main(String[] args) {
+        OffsetTime offset = OffsetTime.now();
+
+        // Current hours
+        int hrs = offset.getHour();
+        MyLogger.consoleLogger.info("Hour: "+ hrs );
+
+        // Current minutes
+        int min = offset.getMinute();
+        MyLogger.consoleLogger.info("Minute: "+ min);
+
+        // Current seconds
+        int sec = offset.getSecond();
+        MyLogger.consoleLogger.info("Second: "+ sec);
+    }
+}
---
+++ JavaPrograms/src/com/nisum/corejava/java8/examples/Timeapi/examples/Zone_Example.java
@@ -0,0 +1,19 @@
+package com.nisum.corejava.java8.examples.Timeapi.examples;
+
+import com.nisum.corejava.util.MyLogger;
+
+import java.time.ZonedDateTime;
+import java.time.Period;
+
+public class Zone_Example {
+    public static void main(String[] args) {
+
+        // get the Time zone configered
+        ZonedDateTime zone = ZonedDateTime.now();
+        MyLogger.consoleLogger.info( zone.getZone() );
+
+        // Get the date after reducing the days
+        ZonedDateTime mi = zone.minus( Period.ofDays( 28 ) );
+        MyLogger.consoleLogger.info( mi );
+    }
+}
