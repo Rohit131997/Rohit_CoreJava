9b869ce51ac152217646adcc6f4d3be9cc891702
Rohit131997 <59564737+Rohit131997@users.noreply.github.com>
2020-02-04 11:54:56 +0530

    Adding

---
+++ JavaPrograms/src/com/nisum/corejava/jdbc/mysql/Jdbc_Exmp1.java
@@ -0,0 +1,53 @@
+package com.nisum.corejava.jdbc.mysql;
+
+//Program of connecting the core java to MySql database
+
+import com.nisum.corejava.util.MyLogger;
+
+//Step 1: importing the package (java.sql.*;)
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+
+public class Jdbc_Exmp1 {
+    public static void main(String[] args) throws Exception {
+
+        String url = "jdbc:mysql://localhost:3306/database";
+        String username = "root";
+        String password = "Rohit@1997";
+        String query;
+        query = "SELECT * FROM database.employee_info where emp_id=101;";
+
+        //Step 2: loading and registering the driver
+        //registering the driver with forName method
+        Class.forName( "com.mysql.jdbc.Driver" );
+
+        //establishing the connection with interface "Connection"
+        Connection con = DriverManager.getConnection( url, username, password );
+
+        //creating the statement
+        Statement st = con.createStatement();
+
+        //processing the results
+        ResultSet rs = st.executeQuery( query );
+
+        rs.next();
+        String name = rs.getString( "emp_name" );
+        int age = rs.getInt( "emp_age" );
+
+        MyLogger.consoleLogger.info(name);
+        MyLogger.consoleLogger.info(age);
+
+        //closing the Connection and Statement
+        st.close();
+        con.close();
+
+
+    }
+
+}
+
+
+
