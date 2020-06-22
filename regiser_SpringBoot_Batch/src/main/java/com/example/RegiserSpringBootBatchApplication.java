f6c28ad6e5c1a40b860ab9144630c0e67a3b7126
Rohit131997 <59564737+Rohit131997@users.noreply.github.com>
2020-06-22 20:26:50 +0530

    Add files via upload

---
+++ regiser_SpringBoot_Batch/src/main/java/com/example/RegiserSpringBootBatchApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegiserSpringBootBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegiserSpringBootBatchApplication.class, args);
+	}
+
+}
---
+++ regiser_SpringBoot_Batch/src/main/java/com/example/processor/PersonItemProcessor.java
@@ -0,0 +1,5 @@
+package com.example.processor;
+
+public class PersonItemProcessor {
+
+}
