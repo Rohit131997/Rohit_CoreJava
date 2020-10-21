bfe293be2393a4af8269ebb7705e10b3a24867bd
Rohit131997 <59564737+Rohit131997@users.noreply.github.com>
2020-10-21 18:07:37 +0530

    Add

---
+++ Java_Practices/src/com/practice/corejava/ReverseEachWord1.java
@@ -0,0 +1,34 @@
+package com.practice.corejava;
+
+public class ReverseEachWord1 {
+    static void reverseEachWordOfString(String inputString) {
+        String[] words = inputString.split(" ");
+
+        String reverseString = "";
+
+        for (int i = 0; i < words.length; i++) {
+            String word = words[i];
+
+            String reverseWord = "";
+
+            for (int j = word.length() - 1; j >= 0; j--) {
+                reverseWord = reverseWord + word.charAt(j);
+            }
+
+            reverseString = reverseString + reverseWord + " ";
+        }
+
+        System.out.println(inputString);
+
+        System.out.println(reverseString);
+
+    }
+
+    public static void main(String[] args) {
+    	
+        reverseEachWordOfString("Hello people");
+
+        reverseEachWordOfString("How are you today");
+
+    }
+}
